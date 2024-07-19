ceb77464f3afaf2ed7c4e8ced5f7e1666435057b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-19 19:14:12 +0300

    working with Jwt in java springboot

--- src/main/java/com/security/security/models/User.java
+++ src/main/java/com/security/security/models/User.java
@@ -1,17 +1,25 @@
 package com.security.security.models;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+@Entity
+@Builder
+@Table (name = "users")
+public class User implements UserDetails {
 
     @Id
     private UUID id;
@@ -19,6 +27,8 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
 
 
@@ -27,5 +37,40 @@ public class User {
         if(this.id == null){
             this.id = UUID.randomUUID();
         }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
