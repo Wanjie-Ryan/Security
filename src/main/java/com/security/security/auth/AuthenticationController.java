456ca2e8abbb66bb4d31a0a4d21b88876c86aa00
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-20 17:00:00 +0300

    working with Jwt in java springboot

--- src/main/java/com/security/security/auth/AuthenticationController.java
+++ src/main/java/com/security/security/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.security.security.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,17 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
 
+    @Autowired
+    private AuthenticationService authService;
 
     @PostMapping("/register")
 
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest req){
-        return null;
+        return ResponseEntity.ok(authService.register(req));
     }
 
     @PostMapping("/login")
 
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest req){
-        return null;
+        return ResponseEntity.ok(authService.authenticate(req));
     }
 
 }
--- src/main/java/com/security/security/auth/AuthenticationService.java
+++ src/main/java/com/security/security/auth/AuthenticationService.java
@@ -1,7 +1,58 @@
 package com.security.security.auth;
 
+import com.security.security.config.JwtService;
+import com.security.security.models.Role;
+import com.security.security.models.User;
+import com.security.security.repository.UserRepository;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Builder
 public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest req) {
+
+        User user = User.builder()
+                .firstname(req.getFirstname())
+                .lastname(req.getLastname())
+                .email(req.getEmail())
+                .password(passwordEncoder.encode(req.getPassword()))
+                .role(Role.User)
+                .build();
+
+
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest req) {
+
+        // the authmanager checks the user authenticity on our behalf, throws an error if the password or email are incorrect
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(req.getEmail(), req.getPassword()));
+
+        var user = userRepo.findByEmail(req.getEmail()).orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
 }
